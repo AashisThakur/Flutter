58a4944397be14931e9d8427e3c132d2f66b885f
AashisThakur <aashishthakur3069@gmail.com>
2023-06-17 21:13:11 +0530

    Flutter demonstration using todo app with search and add delete functionality to understand the basic flow of app and stateless and statefull widgets

---
+++ my_app/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromARGB(205, 225, 21, 21);
+const Color tdBlue = Color.fromARGB(205, 21, 89, 225);
+const Color tdBlack = Color.fromARGB(233, 22, 21, 21);
+const Color tdGrey = Color.fromARGB(205, 69, 67, 67);
+const Color tdBGColor = Color.fromARGB(205, 231, 227, 227);
---
+++ my_app/lib/widgets/todo_item.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:my_app/constants/colors.dart';
+
+import '../model/todo.dart';
+
+class TodoItem extends StatelessWidget {
+  final ToDo todo;
+  final onTodoChanged;
+  final onDeleteItem;
+
+  const TodoItem(
+      {Key? key,
+      required this.todo,
+      required this.onDeleteItem,
+      required this.onTodoChanged})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: 20,
+      ),
+      child: ListTile(
+        onTap: () {
+          onTodoChanged(todo);
+        },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 5,
+        ),
+        tileColor: Colors.white,
+        leading: Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlue,
+        ),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            icon: Icon(Icons.delete),
+            iconSize: 18,
+            color: Colors.white,
+            onPressed: () {
+              onDeleteItem(todo.id);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
